ccef53ddc8999310bf4f02d33d66360420f06537
Fekete Kristóf <h355812>
2024-12-06 00:27:05 +0100

    fel

--- Hostly/src/app/services/http.service.ts
+++ Hostly/src/app/services/http.service.ts
@@ -8,7 +8,7 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  BASE_URL = isDevMode() ? '//localhost:3030/api/' : '/api/'
+  BASE_URL = isDevMode() ? '//http://34.65.143.36/:3030/api/' : '/api/'
   headers = new HttpHeaders({
   'Content-Type': 'application/json',
   'Authorization': 'Bearer <token>',
