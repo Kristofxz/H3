904c53dfcd9e426eefb250e99ee8aeba8eac9a82
Fekete Kristóf <h355812>
2024-12-06 00:29:16 +0100

    fel

--- Hostly/test/test-pages.js
+++ Hostly/test/test-pages.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Main page content', function(done) {
-    request('http://34.65.143.36/:4200' , function(error, response, body) {
+    request('http://34.65.143.36:4200' , function(error, response, body) {
         done();
     });
 });
