769834c7a456f4a0cc9964012491c0a8435ea82e
Fekete Kristóf <h355812>
2024-12-06 00:17:57 +0100

    fel

--- Hostly/src/environments/environment.ts
+++ Hostly/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
       production: false,
-      apiUrl: 'http://34.65.143.36:3030/api/stay/'  // Frissítve a külső IP-vel
+      apiUrl: 'http://34.65.143.36:3030'  // Frissítve a külső IP-vel
     };
